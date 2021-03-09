d1a9eaea88c0678d5366206edd367f9aaffaecfd
Mounir Zelouani <mounirzelouani10@gmail.com>
2021-03-09 10:13:06 +0100

    second commit

---
+++ js/responsive-menu.js
@@ -0,0 +1,9 @@
+function menuBar() {
+   var x = document.getElementById("nav");
+
+   if (x.style.display === "block") {
+      x.style.display = "none";
+   } else {
+      x.style.display = "block"
+   }
+}
